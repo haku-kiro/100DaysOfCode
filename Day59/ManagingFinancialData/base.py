f08f3f18bf498947e7f2270807b57c5a3b80bdaa
haku-kiro <botprime@yahoo.com>
2018-10-05 23:03:11 +0200

    Adding day 61

--- Day59/ManagingFinancialData/base.py
+++ Day59/ManagingFinancialData/base.py
@@ -3,19 +3,19 @@ from pandas_datareader.data import DataReader
 import matplotlib.pyplot as plt
 
 # Set the start date
-start = date(2008, 1, 1)
+start = date(1950, 1, 1)
 
 # Define the series codes
-series = ['BAMLHYH0A0HYM2TRIV', 'SP500']
+series = ['UNRATE', 'CIVPART']
 
 # Import the data
 econ_data = DataReader(series, 'fred', start)
 
 # Assign new column labels
-econ_data.columns = ['Bond','Stock']
+econ_data.columns = ['Unemployment Rate','Participation Rate']
 
 # Plot econ_data
-econ_data.plot(title='Performance Comparison', subplots=True) # subplots renders a plot for each dframe
+econ_data.plot(title='Labor Market') # subplots renders a plot for each dframe
 
 # Show the plot
 plt.show()
