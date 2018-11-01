1a0459566a94f34c6eefd479fbdf94c2587299e0
haku-kiro <botprime@yahoo.com>
2018-11-01 23:16:55 +0200

    Adding day 82 and 83

--- Day78/django-tut/base.py
+++ Day78/django-tut/base.py
@@ -1,1 +1,7 @@
 # django-admin startproject <Site Name> Creates an empty template for your website/app
+# To run the server, use the manage.py file with the runserver arg:
+#python manage.py runserver 
+
+# To create a app, use the following:
+# python manage.py startapp <AppName>
+# Looks as if it creates a db for us ? Creates a webapp folder
